00042c70752a7b8fcc6313cd208da7ba105c4501
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-09 09:45:01 +0300

    Initial commit

---
+++ app/src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<p class="text-red-800">wervfsbgfbgbfdbxgf</p>
